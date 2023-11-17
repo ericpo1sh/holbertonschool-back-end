9c597e9ebae49051b17c1a88d6162ff2834a71b7
ericpo1sh <ericpo1sh@gmail.com>
2023-11-17 12:55:49 -0600

    fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,10 +10,10 @@ def gather_data_from_api(user_id):
     todo_url = f"{url}/todos"
     employee_url = f"{url}/users/{user_id}"
     users_response = requests.get(employee_url).json()
-    employee_data = users_response.json()
-    name = employee_data.get("name")
     todo_response = requests.get(todo_url, params={"userId": user_id}).json()
     todo_data = todo_response.json()
+    employee_data = users_response.json()
+    name = employee_data.get("name")
     fin_tasks = [task["title"] for task in todo_response if task["completed"]]
     completed_tasks = len(fin_tasks)
     total_tasks = len(todo_data)
@@ -26,4 +26,4 @@ def gather_data_from_api(user_id):
 
 if __name__ == "__main__":
     user_id = int(argv[1])
-    gather_data_from_api(int(user_id))
+    gather_data_from_api(user_id)
